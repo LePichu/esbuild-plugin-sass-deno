5ef8f8089715d872258f2ef8b89ca0b26d662c05
Tsukina-7mochi <tsukina7mochi@gmail.com>
2023-03-18 18:23:25 +0900

    fix path resolving

--- mod.ts
+++ mod.ts
@@ -1,12 +1,15 @@
 import { Plugin } from "esbuild"
+import { posix } from "path";
 import sass from "deno's ass"
- 
+
 const sassPlugin: Plugin = {
     name: "esbuild-plugin-sass-deno",
     setup: (build) => {
         build.onLoad({ filter: /\.scss$/ }, async (args) => {
             const file = await Deno.readTextFile(args.path)
-            const css = sass(file).to_string("compressed")
+            const css = sass(file, {
+              load_paths: [posix.dirname(args.path)]
+            }).to_string("compressed")
             return {
                 contents: css.toString(),
                 loader: "text"
