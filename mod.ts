6d0a59cd36f200ada6cf5c0acd526ab9de9f9d2d
Tsukina-7mochi <tsukina7mochi@gmail.com>
2023-03-18 18:24:20 +0900

    apply deno's default code formatter

--- mod.ts
+++ mod.ts
@@ -1,21 +1,21 @@
-import { Plugin } from "esbuild"
+import { Plugin } from "esbuild";
 import { posix } from "path";
-import sass from "deno's ass"
+import sass from "deno's ass";
 
 const sassPlugin: Plugin = {
-    name: "esbuild-plugin-sass-deno",
-    setup: (build) => {
-        build.onLoad({ filter: /\.scss$/ }, async (args) => {
-            const file = await Deno.readTextFile(args.path)
-            const css = sass(file, {
-              load_paths: [posix.dirname(args.path)]
-            }).to_string("compressed")
-            return {
-                contents: css.toString(),
-                loader: "text"
-            }
-        })
-    }
-}
+  name: "esbuild-plugin-sass-deno",
+  setup: (build) => {
+    build.onLoad({ filter: /\.scss$/ }, async (args) => {
+      const file = await Deno.readTextFile(args.path);
+      const css = sass(file, {
+        load_paths: [posix.dirname(args.path)],
+      }).to_string("compressed");
+      return {
+        contents: css.toString(),
+        loader: "text",
+      };
+    });
+  },
+};
 
-export default sassPlugin
+export default sassPlugin;
