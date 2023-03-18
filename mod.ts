6765729a095e4164d3c5224ad17289cc82b79b0c
Tsukina-7mochi <tsukina7mochi@gmail.com>
2023-03-18 20:29:29 +0900

    change loader of css

--- mod.ts
+++ mod.ts
@@ -2,19 +2,55 @@ import { Plugin } from "https://deno.land/x/esbuild@v0.17.11/mod.js";
 import { posix } from "https://deno.land/std@0.174.0/path/mod.ts";
 import sass from "https://deno.land/x/denosass@1.0.6/mod.ts";
 
+const defaultNamespace = "esbuild-plugin-sass-deno";
+const entryNamespace = defaultNamespace + "-entry";
+
+const loadSass = async function (path: string) {
+  const text = await Deno.readTextFile(path);
+
+  return sass(text, {
+    load_paths: [posix.dirname(path)],
+  }).to_string("compressed").toString();
+};
+
 const sassPlugin = (): Plugin => ({
   name: "esbuild-plugin-sass-deno",
   setup: (build) => {
-    build.onLoad({ filter: /\.scss$/ }, async (args) => {
-      const file = await Deno.readTextFile(args.path);
-      const css = sass(file, {
-        load_paths: [posix.dirname(args.path)],
-      }).to_string("compressed");
-      return {
-        contents: css.toString(),
-        loader: "text",
-      };
+    build.onResolve({ filter: /\.scss$/ }, (args) => {
+      if (args.kind === "entry-point") {
+        return {
+          path: args.path,
+          namespace: entryNamespace,
+        };
+      } else {
+        return {
+          path: args.path,
+          namespace: defaultNamespace,
+        };
+      }
     });
+
+    build.onLoad(
+      { filter: /.*/, namespace: defaultNamespace },
+      async (args) => {
+        const cssContent = await loadSass(posix.resolve(args.path));
+        return {
+          contents: cssContent,
+          loader: "css",
+        };
+      },
+    );
+
+    build.onLoad(
+      { filter: /.*/, namespace: entryNamespace },
+      async (args) => {
+        const cssContent = await loadSass(posix.resolve(args.path));
+        return {
+          contents: cssContent,
+          loader: "text",
+        };
+      },
+    );
   },
 });
 
